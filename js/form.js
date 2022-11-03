2a0172cee67e990b7cea2ae89f8b61cbec09cb83
Roman <romansh97@gmail.com>
2022-11-03 15:37:55 +0300

    Добавляет валидацию отдельных полей формы

--- js/form.js
+++ js/form.js
@@ -17,4 +17,28 @@ const activateForm = () => {
   filterOptions.forEach((option) => option.removeAttribute('disabled'));
 };
 
+const pristine = new Pristine(adForm, {
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form__element--invalid',
+  errorTextParent: 'ad-form__element',
+});
+
+const roomNumber = adForm.querySelector('#room_number');
+const guestCapacity = adForm.querySelector('#capacity');
+const capacityOptions = {
+  '100': ['0'],
+  '1': ['1'],
+  '2': ['1', '2'],
+  '3': ['1', '2', '3']
+};
+
+const isEnoughRooms = () => capacityOptions[roomNumber.value].includes(guestCapacity.value);
+
+pristine.addValidator(guestCapacity, isEnoughRooms, 'Не соответствует количеству комнат');
+
+adForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
+
 export {deactivateForm, activateForm};
