2cd30c37964d813543188c1eb91585e4593b9ce1
Roman <romansh97@gmail.com>
2022-11-08 13:59:54 +0300

    Добавляет зависимости полей формы

--- js/form.js
+++ js/form.js
@@ -23,18 +23,62 @@ const pristine = new Pristine(adForm, {
   errorTextParent: 'ad-form__element',
 });
 
-const roomNumber = adForm.querySelector('#room_number');
-const guestCapacity = adForm.querySelector('#capacity');
-const capacityOptions = {
+const roomNumberSelector = adForm.querySelector('#room_number');
+const guestCapacitySelector = adForm.querySelector('#capacity');
+const roomsToPeopleNumber = {
   '100': ['0'],
   '1': ['1'],
   '2': ['1', '2'],
   '3': ['1', '2', '3']
 };
 
-const isEnoughRooms = () => capacityOptions[roomNumber.value].includes(guestCapacity.value);
+const typeToMinPrice = {
+  'bungalow': 0,
+  'flat': 1000,
+  'hotel': 3000,
+  'house': 5000,
+  'palace': 10000
+};
 
-pristine.addValidator(guestCapacity, isEnoughRooms, 'Не соответствует количеству комнат');
+const housingTypeField = adForm.querySelector('#type');
+const priceField = adForm.querySelector('#price');
+
+priceField.min = typeToMinPrice[housingTypeField.value];
+
+const updateMinPrice = () => {
+  priceField.min = typeToMinPrice[housingTypeField.value];
+  priceField.placeholder = typeToMinPrice[housingTypeField.value];
+};
+
+housingTypeField.addEventListener('change', updateMinPrice);
+
+const validatePrice = () => priceField.value.length && +priceField.value >= +priceField.min;
+
+const isEnoughRooms = () => roomsToPeopleNumber[roomNumberSelector.value].includes(guestCapacitySelector.value);
+
+const getPriceWarning = () => `Цена на данный тип объекта не может быть ниже ${priceField.min}`;
+
+pristine.addValidator(guestCapacitySelector, isEnoughRooms, 'Не соответствует количеству комнат');
+
+pristine.addValidator(priceField, validatePrice, getPriceWarning);
+
+const entryTimeField = adForm.querySelector('#timein');
+const exitTimeField = adForm.querySelector('#timeout');
+
+const synchronizeTimes = () => {
+  if (entryTimeField.value < exitTimeField.value) {
+    entryTimeField.value = exitTimeField.value;
+  }
+  if (exitTimeField.value > entryTimeField.value) {
+    exitTimeField.value = entryTimeField.value;
+  }
+  console.log(entryTimeField.value);
+  console.log(exitTimeField.value);
+};
+
+entryTimeField.addEventListener('change', synchronizeTimes);
+
+exitTimeField.addEventListener('change', synchronizeTimes);
 
 adForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
